0dc593282cf8a2e7706263c0f684a721004c9125
Harley Jessop <harley.jessop@verizonmedia.com>
2022-11-16 13:44:43 -0700

    removing dist files

--- dist/components/icon/library.system.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import {
-  library_system_default
-} from "../../chunks/chunk.IZLHTFTU.js";
-import "../../chunks/chunk.LKA3TPUC.js";
-export {
-  library_system_default as default
-};
--- dist/translations/en.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import {
-  en_default
-} from "../chunks/chunk.E66L43KD.js";
-import "../chunks/chunk.LKA3TPUC.js";
-export {
-  en_default as default
-};
