a54fab5264bec50dc84f308e0ef4faaeae2f7efb
Harley Jessop <hjessop@edg.io>
2023-03-08 10:27:42 -0700

    nothin important

--- src/lynk.ts
+++ src/lynk.ts
@@ -64,8 +64,8 @@ export { default as LynkTableHeaderGroup } from './components/table-header-group
 export { default as LynkTableRow } from './components/table-row/table-row';
 export { default as LynkTableRowGroup } from './components/table-row-group/table-row-group';
 export { default as LynkOption } from './components/option/option';
-
 export { default as LynkTruncate } from './components/truncate/truncate';
+
 /* plop:component */
 
 // Utilities
